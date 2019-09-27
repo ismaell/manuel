5186f2f434fe95967552e7f82d0e3d70208de399
Ismael Luceno <ismael@iodev.co.uk>
2019-09-27 22:36:54 +0200

    Add report name customization support

--- manuel/cli.py
+++ manuel/cli.py
@@ -17,7 +17,8 @@ def invoke():
 @click.argument('config_file')
 @click.option('--index/--no-index', default=False)
 @click.option('--debug/--no-debug', default=False)
-def cli_generate_report(config_file, index, debug):
+@click.option('--name')
+def cli_generate_report(config_file, index, debug, name):
     """
     CLI entry point
 
@@ -28,4 +29,7 @@ def cli_generate_report(config_file, index, debug):
     """
     if index:
         create_index(config_file, debug)
-    generate_report(config_file, debug)
+    if not name:
+        from os.path import basename, splitext
+        name = splitext(basename(config_file))[0] or 'report'
+    generate_report(config_file, debug, name)
